8fb3354d729fd46fdb85e03f0875e1cbbe6b42c7
Sofia <Your-Email>
2021-04-06 01:36:57 -0700

    Sofia_intallation and component built

---
+++ group2/Sofia_comps/textbox/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+cosnt TextboxInput = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ebebebc2;
+    border-radius: 10px;
+    width: 315px;
+    height: 128px;
+`;
+
+const Textbox = () => {
+    return
+    <div>
+        <TextboxInput/>
+    </div>
+
+export default Textbox;
---
+++ group2/stories/textbox.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Textbox from '../Sofia_comps/textbox';
+
+export default {
+    title: "Textbox",
+    component: <Textbox/>
+}
+
+export const myTextbox = () => <div>text</div>
