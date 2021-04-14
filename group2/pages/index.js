29efcd182dbd178e86ddbf616b6d54ef72c23661
GlianBCIT <gpalma1@my.bcit.ca>
2021-04-14 16:57:56 -0700

    changed files

--- group2/pages/index.js
+++ group2/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 // import styles from '../styles/global.css'
-import Button from '../comps/button';
+import Button from '../comps/Quesbutton';
 import Nav from '../comps/nav';
 import Back from '../comps/back';
 import styled from 'styled-components';
