4df12e15c8688196c60c6f88579f2736dcc37293
GlianBCIT <gpalma1@my.bcit.ca>
2021-04-16 16:01:55 -0700

    Changed Arrow Icon

--- group2/comps/back/index.js
+++ group2/comps/back/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-import Arrow from '../../images/BackArrow.svg';
+import {BiArrowBack} from 'react-icons/bi';
 
 const BackCont = styled.div`
     display: flex;
     flex-direction: row;
 `;
-const BackImg = styled.image`
+const BackImg = styled.div`
 display: inline-block;
 width: 24px;
 height: 24px;
@@ -23,12 +23,12 @@ bottom: 3px;
 `;
 
 const Back = ({
-
+width=24
 }) => {
 
     return <BackCont>
         <BackImg>
-            <img src={Arrow} alt="Arrow Icon"/>
+        <BiArrowBack color="#8A8A8A" />
         </BackImg>
         <Text>BACK</Text>
     </BackCont>
