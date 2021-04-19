effae77587e4e3810bde3230e2fbf9e2a917889e
GlianBCIT <gpalma1@my.bcit.ca>
2021-04-18 19:27:45 -0700

    Changes

--- group2/comps2/navbutton.js
+++ group2/comps2/navbutton.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import {useRouter} from 'next/router';
 
 const ButtonCont = styled.span`
   margin:20px;
 `;
-
 const ButtonInput = styled.button`
       background-color:${props=>props.bg};
       color:${props=>props.textcolor};
@@ -18,19 +17,19 @@ const ButtonInput = styled.button`
 `;
 
 const NavyButton = ({
-
   text="Next",
   bgcolor="#26325B", 
   color = "#FFF",
   border = "none"
 
 }) => {
+  const router =useRouter(); 
   return <ButtonCont>
+    {/* <ButtonImg src="/vercel.svg"/> */}
 
     <ButtonInput bg={bgcolor} textcolor={color} navyborder={border}>
       {text}
     </ButtonInput>
-    
   </ButtonCont>
 }
 
---
+++ group2/pages/start2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import MinWhitebg from '../comps/WhiteBG';
+import Textbox from '../comps/textbox';
+import Start from '../comps/NavyButton';
+
+export default function Started(){
+    
+}
