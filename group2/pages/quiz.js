0b49dedec3acbb84721b076445ce6f94fbfc0019
Sofia <Your-Email>
2021-04-18 18:54:25 -0700

    sofia-pages trouble

---
+++ group2/pages/quiz.js
@@ -0,0 +1,17 @@
+//import style
+import styled from 'styled-componenets';
+
+//import comps
+import NavyButton from '../comps/NavyButton';
+import DefaultNav from '../comps/nav';
+
+//building page
+export default function Quiz (){
+    return <div>
+
+        <DefaultNav>
+        </DefaultNav>
+        <NavyButton></NavyButton>
+
+    </div>
+}
