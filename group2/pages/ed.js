782eee15c9d968ff830c17597fc83bdc5691d012
juuheek <jkim844@my.bcit.ca>
2021-04-19 21:20:26 -0700

    Juhee_hello

--- group2/pages/ed.js
+++ group2/pages/ed.js
@@ -47,7 +47,7 @@ export default function Glian(){
     <Back routeTo="/disclaimer"/>
     </BackDiv>
   
-        <Resources onLeftClick={()=>HandleText("Anorexia Nervosa","is a psychological and potentially life-threatening eating disorder.Those suffering from this are typically suffering from an extremely low body weight relative to their height and body type.")} onRightClick={()=>HandleText("")}></Resources>
+        <Resources onLeftClick={()=>HandleText("Anorexia Nervosa","is a psychological and potentially life-threatening eating disorder.Those suffering from this are typically suffering from an extremely low body weight relative to their height and body type.")} onRightClick={()=>HandleText("Good Eating Habits","To maintain a generally healthy lifestyle, eating is important! It provides our body with the nutrients it needs to get through the day. Click below to read more on good eating habits you can start using yourself.")}></Resources>
         <Cards></Cards>
         <Row>
 
