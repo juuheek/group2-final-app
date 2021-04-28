a69e19f26d9f481e4d6706b0c21b32bc03a0e26d
juuheek <jkim844@my.bcit.ca>
2021-04-27 21:59:31 -0700

    Juhee_STATE WORKS!!!!

--- group2/pages/resources.js
+++ group2/pages/resources.js
@@ -10,23 +10,86 @@ import SymptomCard from '../comps/JuheeSymptomCard'
 import BottomMenu from '../comps/BottomMenu'
 import LeftArrow from '../comps/JuheeLeftArrow'
 import RightArrow from '../comps/JuheeRightArrow'
-
 import React, {useState} from 'react';
+const data = {
+  Anorexia:{
+    explanation:"Anorexia",
+    explanationtext:"a psychological and potentially life-threatening eating disorder. Those suffering from this are typically suffering from an extremely low body weight relative to their height and body type.",
+    cause:["Thinness culture in media","Professions and careers that promote being thin","Family and childhood traumas"],
+    symptom:["Extremely restrictive eating","Intensive/ excessive exercise","Extreme thinness"],
+    piedegree:"conic-gradient(#26325B 40deg, #E67571 0 235deg, #E67571 0)",
+    pietext1:"Eating Disorders mortality rate is ",
+    piepercent:"10-15%",
+    pietext2:"which is the highest mortality rate of any mental illess",
+    bgcolor:"#FFCCB2"
 
+  },
+  Bulimia:{
+    explanation:"Bulimia Nervosa",
+    explanationtext:"a psychological and severe life-threatening eating disorder described by the ingestion of an abnormally large amount of food in short time period, followed by an attempt to avoid gaining weight by purging what was consumed.",
+    cause:["Stressful transitions or life changes","History of abuse or trauma","Negative body image"],
+    symptom:["Chronically inflamed and sore throat ","Swollen salivary glands (by neck and jaw)","Reflux disorder/other gastrointestinal problems"],
+    piedegree:"conic-gradient(#26325B 110deg, #E67571 0 250deg, #E67571 0)",
+    pietext1:"Up to",
+    piepercent:"33-70%",
+    pietext2:"of those who have bulimia also have an addictive disorder",
+    bgcolor:"#F8E8AE"
+
+  },
+  Orthorexia:{
+    explanation:"Orthorexia",
+    explanationtext:"a common co-occurring eating disorder. It is characterized by a fixation or desire to eat only “healthy” foods, or to avoid entire food groups. Every person suffering with orthorexia has their own food preferences; foods they will eat and foods that they won’t. Orthorexics feel isolated from or superior to people who may consume a food they they themselves reject.",
+    cause:["Past history of trauma","perfectionism tendencies","current or former eatind disorders"],
+    symptom:["An increase in concern about the health of ingredients","showing high levels of stress if “safe” or “healthy” food are not available,","inability to eat anything but a narrow group of foods deemed “pure” or “healthy”"],
+    piedegree:"conic-gradient(#26325B 90deg, #E67571 0 270deg, #E67571 0)",
+    pietext1:"a study has found that",
+    piepercent:"25%",
+    pietext2:"of students attending university experience symptoms of orthorexia",
+    bgcolor:"#FFC1C2"
+
+  },
+  Binge:{
+    explanation:"Binge Eating (BED)",
+    explanationtext:"is commonly known by compulsive overeating or consuming abnormal amounts of food while feeling unable to stop and a loss of control. Binge eating episodes are typically classified as occurring on average a minimum of twice per week for a duration of six months.",
+    cause:["Depression","The causes are unknown however genetics, biological factrs, long term dieting and psychological issues increase risk","The causes are unknown however genetics, biological factrs, long term dieting and psychological issues increase risk"],
+    symptom:["Feeling that your eating behavior is out of control","out of control eating behavior","eating when full or not hungry"],
+    piedegree:"conic-gradient(#26325B 108deg, #E67571 0 252deg, #E67571 0)",
+    pietext1:"Up to",
+    piepercent:"30%",
+    pietext2:"of those looking into weight loss treatments likely show symptoms of BED",
+    bgcolor:"#FFC1C2"
+
+  },
+  ExcessiveExercise:{
+    explanation:"Excessive Exercise",
+    explanationtext:"Exercise and nutrition are foundational to good health, but extreme behaviors can be a red flag indicating unhealthy behaviors. Examples of this include excessive exercise, weight obsessions, bulimia, anorexia and binge eating disorders.",
+    cause:["Physical 'burnout' ","body disphoris/body image disorder","unhealthy obsession/addiction with excercise"],
+    symptom:["Feeling sore muscles or heavy limbs.","reducing other sctivites to make more time for exercising","inabiity to stick to reduced workout routines"],
+    piedegree:"conic-gradient(#26325B 144deg, #E67571 0 216deg, #E67571 0)",
+    pietext1:"out of those who suffer from an eating disorder",
+    piepercent:"40%",
+    pietext2:"also suffer from compulsive exercise",
+    bgcolor:"#FFC1C2"
+
+  }
+}
 const JuheeComps = styled.div`
-
+  
+    ${'' /* background-color:#E8ECF0;
+    padding:20px; */}
    
 `;
 const JuheeCont = styled.div`
     width:414px;
-    max-height:1483px;
+    max-height:1600px;
     background-color:${props=>props.bgcolor};
     display:flex;
     flex-direction:column;
+    ${'' /* justify-content:center; */}
     align-items:center;
 `;
 const JuheeDiv =styled.div`
-    margin:20px;
+    margin:30px;
 `;
 const JuheeDiv2 = styled.div`
   margin:15px;
@@ -38,7 +101,6 @@ font-family: DIN alternate;
 color:#26325B;
 `;
 const CauseCardImg = styled.img`
-
 padding:0 20px 0 20px;
 `;
 const MiddleIcons = styled.div`
@@ -51,120 +113,89 @@ display:flex;
 
 export default function Resources () {
 
-  const [causetext, bottomcausetext] = useState ("Thinness culture in media");
-  const HandleCauseLeft = () => {
-    console.log("clicked")
-        if(cause === "Thinness culture in media"){
+  // const [causetext, bottomcausetext] = useState ("Thinness culture in media");
+  // const HandleCauseLeft = () => {
+  //   bottomcausetext(" Professions and careers that promote being thin");
+  // }
+  // const HandleCauseRight = () => {
+  //   bottomcausetext(" Family and childhood traumas");
+  // }
 
-          bottomcausetext (" Professions and careers that promote being thin") 
-        } else if (cause === "Stressful transitions or life"){
-          bottomcausetext (" History of abuse or trauma") 
-        } else if (cause === "Past history of trauma"){
-          bottomcausetext("perfectionism tendencies")
-        }else if (cause === "Depression"){
-          bottomcausetext("The causes are unknown however genetics, biological factrs, long term dieting and psychological issues increase risk")
-        }else if (cause === "burnout"){
-          bottomcausetext("body disphoris/body image disorder ")
-        }
-    
-  }
-  const HandleCauseRight = () => {
-    console.log("clicked")
-    if(cause === "Thinness culture in media"){
+  // const [symptomtext, bottomsymptomtext] = useState ("Extremely restrictive eating");
+  // const [symptomimg, symptomiconimg] = useState ("/food.svg");
+  // const HandleSymptomLeft = () => {
+  //   bottomsymptomtext(" Intensive/excessive exercise");
+  //   symptomiconimg("/exercise.svg")
+  // }
+  // const HandleSymptomRight = () => {
+  //   bottomsymptomtext("Extreme thinness");
+  // }
 
-      bottomcausetext(" Family and childhood traumas");
-    }
+const [key, setKey] = useState("Anorexia")
+const [sub_ind, setSubInd] = useState(0);
+const [sub_ind2, setSubInd2] = useState(0);
 
-  }
-
-  const [symptomtext, bottomsymptomtext] = useState ("Extremely restrictive eating");
-  const [symptomimg, symptomiconimg] = useState ("/food.svg");
-  const HandleSymptomLeft = () => {
-    console.log("clicked")
-    bottomsymptomtext(" Intensive/excessive exercise");
-    symptomiconimg("/exercise.svg")
-  }
-  const HandleSymptomRight = () => {
-    console.log("clicked")
-    bottomsymptomtext("Extreme thinness");
-  }
-
-const [explanation, explanationbold] = useState ("Anorexia");
-const [explanationtext, explanationregular] = useState ("a psychological and potentially life-threatening eating disorder. Those suffering from this are typically suffering from an extremely low body weight relative to their height and body type. ");
-const [cause, defaultcause] =useState("Thinness culture in media");
-const [symptom, defaultsymptom] =useState("Extremely restrictive eating");
-const [piedegree, piechartdegree] = useState("conic-gradient(#26325B 40deg, #E67571 0 235deg, #E67571 0)")
-const [pietext1, piecharttext1] = useState("Eating Disorders mortality rate is ")
-const [piepercent, piechartpercent]=useState("10-15%")
-const [pietext2, piecharttext2] =useState("which is the highest mortality rate of any mental illess")
-const [bgcolor, setbgcolor] = useState ("#FFCCB2") 
-
-
-const HandleExplanation = (
-  title="Anorexia",
-  text="a psychological and potentially life-threatening eating disorder. Those suffering from this are typically suffering from an extremely low body weight relative to their height and body type.",
-  bottomtxt="Thinness culture in media",
-  bottomtxt2 = "Extremely restrictive eating ",
-  mypiedegree="conic-gradient(#26325B 40deg, #E67571 0 235deg, #E67571 0)",
-  mypietext1="Eating Disorders mortality rate is ",
-  mypiepercent="10-15%",
-  mypietext2="which is the highest mortality rate of any mental illess",
-  bgcolor="#FFCCB2"
-
-  
-) =>{
-  explanationbold(title)
-  explanationregular(text)
-  defaultcause(bottomtxt)
-  defaultsymptom(bottomtxt2)
-  piechartdegree(mypiedegree)
-  piecharttext1(mypietext1)
-  piecharttext2(mypietext2)
-  piechartpercent(mypiepercent)
-  setbgcolor(bgcolor)
-
-
-  console.log("click")
+const HandleExplanation = (k) =>{
+setKey(k);
 }
 
+const HandleCauseCard = (ind) => {
+  if(ind < 0){
+    ind=0;
+  }
+  if(ind > 2){
+    ind=2;
+  }
+  setSubInd(ind)
+}
+const HandleSymptomCard = (ind2) => {
+  if(ind2 < 0){
+    ind2=0;
+  }
+  if(ind2 > 2){
+    ind2=2;
+  }
+  setSubInd2(ind2)
+}
 
   return (
     <JuheeComps >
     
-    <JuheeCont bgcolor={bgcolor} >
+    <JuheeCont bgcolor={data[key].bgcolor} >
       <NavBar 
-      onTxt2Click={()=>HandleExplanation("Bulimia Nervosa", "a psychological and severe life-threatening eating disorder described by the ingestion of an abnormally large amount of food in short time period, followed by an attempt to avoid gaining weight by purging what was consumed.","Stressful transitions or life ","Chronically inflamed and sore throat ","conic-gradient(#26325B 110deg, #E67571 0 250deg, #E67571 0)","Up to","33-70%","of those who have bulimia also have an addictive disorder", "#F8E8AE")} 
+      onTxt2Click={()=>HandleExplanation("Bulimia")} 
       
       onTxt1Click={()=>HandleExplanation("Anorexia")} 
       
-      onTxt3Click={()=>HandleExplanation("Orthorexia","a common co-occurring eating disorder. It is characterized by a fixation or desire to eat only “healthy” foods, or to avoid entire food groups. Every person suffering with orthorexia has their own food preferences; foods they will eat and foods that they won’t. Orthorexics feel isolated from or superior to people who may consume a food they they themselves reject.","Past history of trauma","An increase in concern about the health of ingredients", "conic-gradient(#26325B 90deg, #E67571 0 270deg, #E67571 0)","a study has found that","25%","of students attending university experience symptoms of orthorexia","#FFC1C2")} 
+      onTxt3Click={()=>HandleExplanation("Orthorexia")} 
       
-      onTxt4Click={()=>HandleExplanation("Binge Eating (BED)","is commonly known by compulsive overeating or consuming abnormal amounts of food while feeling unable to stop and a loss of control. Binge eating episodes are typically classified as occurring on average a minimum of twice per week for a duration of six months.","Depression","Feeling that your eating behavior is out of control","conic-gradient(#26325B 108deg, #E67571 0 252deg, #E67571 0)","Up to","30%","of those looking into weight loss treatments likely show symptoms of BED","#FFC1C2")} 
+      onTxt4Click={()=>HandleExplanation("Binge")} 
       
-      onTxt5Click={()=>HandleExplanation("Excessive Exercise","Exercise and nutrition are foundational to good health, but extreme behaviors can be a red flag indicating unhealthy behaviors. Examples of this include excessive exercise, weight obsessions, bulimia, anorexia and binge eating disorders.","Physical 'burnout' ","Feeling sore muscles or heavy limbs." ,"conic-gradient(#26325B 144deg, #E67571 0 216deg, #E67571 0)","out of those who suffer from an eating disorder","40%","also suffer from compulsive exercise","#FFC1C2")}  routeTo="/ed"/>
+      onTxt5Click={()=>HandleExplanation("ExcessiveExercise")}  routeTo="/ed"/>
+      
       <JuheeDiv>
-        <Whitebg  bold={explanation} text={explanationtext}/>
+      <Whitebg  bold={data[key].explanation} text={data[key].explanationtext}/>
       </JuheeDiv>
       <JuheeDiv>
       <JuheeDiv2>
       <JuheeH3>Potential Causes</JuheeH3>
       <MiddleIcons>
-      <LeftArrow onLeftClick ={HandleCauseLeft}   />
-      <CauseCard bottomcausetext={causetext} bottomcausetext={cause} />
-      <RightArrow  onRightClick ={HandleCauseRight}  />
+      <LeftArrow onLeftClick ={()=>HandleCauseCard(sub_ind-1)}  />
+      <CauseCard  bottomcausetext={data[key].cause[sub_ind]} />
+      <RightArrow  onRightClick ={()=>HandleCauseCard(sub_ind+1)}   />
       </MiddleIcons>
       </JuheeDiv2>
       <JuheeDiv2>
       <JuheeH3>Symptoms</JuheeH3>
       <MiddleIcons>
-      <LeftArrow onLeftClick ={HandleSymptomLeft}  />
-      <SymptomCard bottomsymptomtext={symptom}/>
-      <RightArrow  onRightClick ={HandleSymptomRight} />
+      <LeftArrow onLeftClick ={()=>HandleSymptomCard(sub_ind2-1)}/>
+      <SymptomCard  bottomsymptomtext={data[key].symptom[sub_ind2]} />
+      <RightArrow onRightClick ={()=>HandleSymptomCard(sub_ind2+1)}   />
       </MiddleIcons>
       </JuheeDiv2>
       </JuheeDiv>
       <JuheeDiv>
-        <PieChart degree={piedegree} text1={pietext1} percentage={piepercent} text2={pietext2} />
+        <PieChart degree={data[key].piedegree} text1={data[key].pietext1} percentage={data[key].piepercent} text2={data[key].pietext2} />
       </JuheeDiv>
       <JuheeDiv>
       <JuheeDiv2>
@@ -175,13 +206,11 @@ const HandleExplanation = (
       </JuheeDiv2>
       </JuheeDiv>
       <BottomMenu/>
-      
     </JuheeCont>
   
-    
+  
         
     </JuheeComps>
-
 
 
 
